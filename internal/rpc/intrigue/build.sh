b362d5298cd97c7d2a6ef48256f40aa08caac8fa
Abraham Dick <abedick@ku.edu>
2019-04-04 09:46:26 -0500

    added js proto out

--- internal/rpc/intrigue/build.sh
+++ internal/rpc/intrigue/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
+# Go
 protoc --go_out=plugins=grpc:. *.proto
 
 cd ../
+# Python
 python -m grpc_tools.protoc -I./intrigue --python_out=. --grpc_python_out=. ./intrigue/intrigue.proto
 mv *.py ./intrigue/
 
-## Python Deps
-# sudo python -m pip install grpcio
-# sudo python -m pip install grpcio-tools
+# Node Version
+protoc --js_out=import_style=commonjs,binary:./ intrigue/intrigue.proto
